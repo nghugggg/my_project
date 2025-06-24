786010fa73cce4aae2e1c80c5a90577f35bf0209
nnh2706 <hungnguyen270604@gmail.com>
2025-06-25 01:39:06 +0700

    lưu thay đổi

--- resources/views/user/checkout_page.blade.php
+++ resources/views/user/checkout_page.blade.php
@@ -82,7 +82,8 @@
         </div>
     </div>
     <div class="container">
-        <div class="d-flex justify-content-between align-items-center mb-3">
+        <form action="#" class="checkout__form">
+            <div class="d-flex justify-content-between align-items-center mb-3">
                 <h5 class="mb-0">Địa chỉ nhận hàng</h5>
                 <button class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#addAddressModal">
                     + Thêm địa chỉ
@@ -103,7 +104,6 @@
                     </div>
                 @endif
             </div>
-        <form action="#" class="checkout__form">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="col-lg-12">
